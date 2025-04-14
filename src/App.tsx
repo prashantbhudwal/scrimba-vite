0028dcec36bfaaa708f431e13b3b9ceec27b7aba
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-04-14 12:19:24 +0530

    LO: adding a plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/header";
+import ReactLogo from "./assets/react.svg?react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,7 +15,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <ReactLogo />
         </a>
       </div>
       <h1>Vite + React</h1>
